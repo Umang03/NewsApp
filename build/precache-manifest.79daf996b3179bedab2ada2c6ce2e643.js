39f4291694a5bbd3cc8464291b6be112b123162c
Umang <umang.mait@gmail.com>
2022-01-18 20:29:37 +0530

    commit

---
+++ build/precache-manifest.79daf996b3179bedab2ada2c6ce2e643.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682508bed6571e5ed8a67bc76e1a9587",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33856cf3adc5d51637fb",
+    "url": "/static/css/main.47fdc866.chunk.css"
+  },
+  {
+    "revision": "ae880c4d961e8766c44b",
+    "url": "/static/js/2.12d7a76f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.12d7a76f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33856cf3adc5d51637fb",
+    "url": "/static/js/main.0aed1ac2.chunk.js"
+  },
+  {
+    "revision": "951152b74b036bd0247d",
+    "url": "/static/js/runtime-main.6b02212a.js"
+  }
+]);
